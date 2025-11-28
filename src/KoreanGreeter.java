39abbc74b73a32f41924e20dd013b92b7e19856b
czvvet <bsi25031@uhe.ac.jp>
2025-11-28 14:29:24 +0900

    言語の追加

---
+++ src/KoreanGreeter.java
@@ -0,0 +1,7 @@
+
+public class KoreanGreeter implements Greetable{
+	
+	public void greet() {
+		System.out.println("안녕하세요");
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,12 @@ public class Main {
 	public static void main(String[] args) {
 		Greetable g1 = new EnglishGreeter();
 		Greetable g2 = new JapaneseGreeter();
+		Greetable g3 = new KoreanGreeter();
 		
 		g1.greet();
 		g2.greet();
+		g3.greet();
+		
 
 	}
 
